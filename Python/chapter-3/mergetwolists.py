a3f445886f3721f27776a15e536594d85be6260d
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2023-08-10 11:07:34 +0530

    modify mergetwosortedlist

--- Python/chapter-3/mergetwolists.py
+++ Python/chapter-3/mergetwolists.py
@@ -3,27 +3,47 @@ class Node:
         self.data: Node | None = data
         self.next: Node | None = None
 
-class LinkedList:
-    def __init__(self):
-        self.head: Node | None = None
+def mergeTwoSortedList(l1, l2):
+    dummy = Node(None)
+    tail = dummy
 
-    def Solution(self, l1, l2):
-        dummy = Node(0)
-        tail = dummy
-
-        while l1 and l2:
-            if l1.data > l2.data:
-                tail.next = l2
-                l2 = l2.next
-            else:
-                tail.next = l1
-                l1 = l1.next
-            tail = tail.next
-
-        if not l1:
-            tail.next = l2
-        else:
+    while l1 and l2:
+        if l1.data < l2.data:
             tail.next = l1
+            l1 = l1.next
+        else:
+            tail.next = l2
+            l2 = l2.next
+        tail = tail.next
 
-        self.head = dummy.next
+    if not l1:
+        tail.next = l2
+    else:
+        tail.next = l1
 
+    return dummy.next
+
+def display(head):
+    current = head
+    while current:
+        print(current.data, end=" -> ")
+        current = current.next
+    print("None")
+
+node1 = Node(1)
+node3 = Node(3)
+node5 = Node(5)
+node1.next = node3
+node3.next = node5
+
+# List 2: 2 -> 4 -> 6
+node2 = Node(2)
+node4 = Node(4)
+node6 = Node(6)
+node2.next = node4
+node4.next = node6
+
+# Merge and print merged sorted linked list
+merged_head = mergeTwoSortedList(node1, node2)
+print("Merged sorted linked list:")
+display(merged_head)
