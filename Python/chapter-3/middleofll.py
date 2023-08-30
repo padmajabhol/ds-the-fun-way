00fb3c8579e9379cff8b9f8fbe2d9470deec5e4d
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2023-08-30 17:39:49 +0530

    Update middleofll.py

--- Python/chapter-3/middleofll.py
+++ Python/chapter-3/middleofll.py
@@ -1,52 +1,24 @@
-import math
-class Node():
-    def __init__(self, data):
-        self.data: Node | None = data
-        self.next: Node | None = None
+class Node:
+    def __init__(self, val = 0, next = None):
+        self.val = val
+        self.next = next
 
-class SingleLinkedList():
+def find_middle(head):
+    if head is None:
+        return None
 
-    def __init__(self):
-        self.head: Node | None = None
+    slow = fast = head
+    while fast is not None and fast.next is not None:
+        slow = slow.next
+        fast = fast.next.next
+    return slow.val
 
-    def middle(self):
-        length = 0
-        temp = self.head
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(3)
+head.next.next.next = ListNode(4)
+head.next.next.next.next = ListNode(5)
 
-        while temp:
-            length = length + 1
-            temp = temp.next
-
-        mid_idx = math.ceil(length/2)
-
-        if length % 2 == 0:
-            mid_idx = mid_idx + 1
-
-        mid_node = self.head
-
-        for _ in range(mid_idx - 1):
-            mid_node = mid_node.next
-        return mid_node.data
-
-    def display(self):
-        if self.head is None:
-            print("Linked List is empty")
-        else:
-            temp = self.head
-            while temp:
-                print(temp.data, "-->", end=" ")
-                temp = temp.next
-
-L = SingleLinkedList()
-n = Node(10)
-L.head = n
-n1 = Node(20)
-n.next = n1
-n2 = Node(30)
-n1.next = n2
-n3 = Node(40)
-n2.next = n3
-n4 = Node(50)
-n3.next = n4
-L.display()
-print(L.middle())
+middle = find_middle(head)
+    
+        
